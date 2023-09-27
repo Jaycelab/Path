c5b6fa892fb6a0728cdc5daf3a8f932d4678b431
Jason Alvarado <jayxalvarado@gmail.com>
2023-09-26 20:15:57 -0700

    updated fizzbizz example

--- Notes/javascript/examples/angela.js
+++ Notes/javascript/examples/angela.js
@@ -108,10 +108,24 @@ if (guestList.includes(name)) {
 } else {
   alert("You are not welcome");
 }
-for (var i = 1; i < 101; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
-}
+
+
+
 */
+
+function fizzBuzz() {
+	for (let i = 1; i < 101; i++) {
+		
+		if (i % 15 === 0) {
+        console.log("FizzBuzz")}
+		else if (i % 5 === 0 ) {
+		    console.log('Bizz')}
+	  else if (i % 3 === 0 ) {
+	console.log('Fizz')}
+    else {
+      console.log(i)
+    }
+  }
+}
+
+fizzBuzz();
